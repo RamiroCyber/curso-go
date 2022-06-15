ccd2dd6c36e658021dd2d16029de5cfbbf9bb7fe
RamiroCyber <ramiro.dr@gmail.com>
2022-06-15 11:14:17 -0300

    slice

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá mundo")
+	card := []string{"Ace of Diamonds", newCard()}
+
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
